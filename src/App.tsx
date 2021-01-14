1b79b7613941acd8fe24b7fef8c3979a4bfcda72
susiyaki <susiyaki.dev@gmail.com>
2021-01-14 21:54:47 +0900

    setup ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import {HashRouter, Switch, Route} from 'react-router-dom';
 import './App.css';
 
-function App() {
+type Props = Record<string, unknown>;
+
+const App: React.FC<Props> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/">
+          <div>sample</div>
+        </Route>
+      </Switch>
+    </HashRouter>
   );
-}
+};
 
 export default App;
