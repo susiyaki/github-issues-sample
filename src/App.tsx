45747f42d69853cad129f3e87a70006266563d30
susiyaki <39510845+susiyaki@users.noreply.github.com>
2021-01-15 00:34:52 +0900

    issues 2 (#9)
    * add issues page
    
    * get issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import {HashRouter, Switch, Route} from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {HashRouter, Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
+import {route} from './config/route';
+import {Issues} from './pages/issues';
 
 type Props = Record<string, unknown>;
 
+const queryClient = new QueryClient();
+
 const App: React.FC<Props> = () => {
   return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/">
-          <div>sample</div>
-        </Route>
-      </Switch>
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <Switch>
+          <Route exact path={route.issues} component={Issues} />
+          <Route render={() => <Redirect to={route.issues} />} />
+        </Switch>
+      </HashRouter>
+    </QueryClientProvider>
   );
 };
 
