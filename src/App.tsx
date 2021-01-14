be0ece546fcc8e698d4ef6d4ed9c706d2821f6ee
susiyaki <susiyaki.dev@gmail.com>
2021-01-15 00:30:41 +0900

    get issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import {HashRouter, Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 import {route} from './config/route';
@@ -6,14 +7,18 @@ import {Issues} from './pages/issues';
 
 type Props = Record<string, unknown>;
 
+const queryClient = new QueryClient();
+
 const App: React.FC<Props> = () => {
   return (
-    <HashRouter>
-      <Switch>
-        <Route exact path={route.issues} component={Issues} />
-        <Route render={() => <Redirect to={route.issues} />} />
-      </Switch>
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <Switch>
+          <Route exact path={route.issues} component={Issues} />
+          <Route render={() => <Redirect to={route.issues} />} />
+        </Switch>
+      </HashRouter>
+    </QueryClientProvider>
   );
 };
 
--- src/pages/issues/index.tsx
+++ src/pages/issues/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import {useIssueApi} from '../../hooks/api/issue';
 
 type Props = Record<string, unknown>;
 
 export const Issues: React.FC<Props> = () => {
+  const {getIssues} = useIssueApi();
+
+  const {data: issues, status} = getIssues({offset: 0, limit: 10});
+
+  if (status === 'loading') {
+    return (
+      <div>
+        <p>loading...</p>
+      </div>
+    );
+  }
   return (
     <div>
-      <p>sample</p>
+      {issues?.map((issue) => (
+        <div key={issue.id}>
+          <p>{issue.id}</p>
+          <p>{issue.title}</p>
+          <div style={{border: '1px solid #000', width: '100%'}} />
+        </div>
+      ))}
     </div>
   );
 };
