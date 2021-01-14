2cdc503b77447e6b6102c6e04bdefdbf2de83cbc
susiyaki <susiyaki.dev@gmail.com>
2021-01-14 21:54:47 +0900

    setup ci/cd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,45 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-    "plugin:jest/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
   ],
-  plugins: ["jest"],
+  plugins: ['jest'],
   env: {
-    "jest/globals": true,
+    'jest/globals': true,
     browser: true,
   },
   rules: {
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error",
-    "@typescript-eslint/explicit-module-boundary-types": "warn",
-    "no-unused-vars": "warn",
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    'no-unused-vars': 'warn',
   },
   overrides: [
     {
       // enable the rule specifically for TypeScript files
-      files: ["*.ts", "*.tsx"],
+      files: ['*.ts', '*.tsx'],
       rules: {
-        "@typescript-eslint/explicit-module-boundary-types": ["warn"],
+        '@typescript-eslint/explicit-module-boundary-types': ['warn'],
       },
     },
   ],
